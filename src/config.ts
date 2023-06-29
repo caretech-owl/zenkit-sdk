5c1e94ba642bb106f2e9993dba263ab189186512
Alexander Neumann <aleneum@gmail.com>
2023-06-29 18:23:51 +0200

    setup project

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const API_KEY = process.env.ZENKIT_API_KEY;
+export const BASE_URL = "https://base.zenkit.com/api/v1";
+
+axios.interceptors.request.use(function (config) {
+  config.headers["Content-Type"] = "application/json";
+  config.headers["Zenkit-API-Key"] = API_KEY;
+  return config;
+});
