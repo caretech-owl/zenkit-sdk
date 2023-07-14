a7e719fe8747e4967a1c2c372dacf22ae88f9433
Alexander Neumann <aleneum@gmail.com>
2023-07-14 10:15:34 +0200

    convenience re-export from index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { default as Zenkit } from "./zenkit";
+export { Collection as Collection } from "./collection";
+export { ICollection as ICollection } from "./collection";
+export { IEntry as IEntry } from "./entry";
+export { Entry as Entry } from "./entry";
+export { Element as Element } from "./element";
